a91d1d100d642b687ffb2824289b7a5123c64ffd
Patryk Kawiak <patryk.kawiak@wp.pl>
2022-08-22 16:29:17 +0200

    second commit

--- js/main.js
+++ js/main.js
@@ -2,19 +2,22 @@ function roadCost() {
     let distance = document.querySelector('#distance').value;
     let combustion = document.querySelector('#combustion').value;
     let price = document.querySelector('#price').value;
+    let passenger = document.querySelector('#passanger').value;
+    let weight = document.querySelector('#weight').value;
 
+    let addCombustion = weight * passenger * 0.6 / 100;
 
     let combustionPerKm = combustion / 100;
 
     let combustionPerDistance = combustionPerKm * distance;
-    let finalPrice = combustionPerDistance * price;
+    let finalCombustion = addCombustion + combustionPerDistance;
+    let finalPrice = finalCombustion * price;
 
-    if (distance == "" || combustion == "" || price == "") {
+    if (distance == "" || combustion == "" || price == "" || passenger == "" || weight == "") {
         document.querySelector('.result').innerHTML = `Wprowadź wartości`;
     } else {
         document.querySelector('.result').innerHTML = `Koszt przejechania ${distance}km wynosi ${finalPrice.toFixed(2)}zł`;
     }
-
 
 
 
